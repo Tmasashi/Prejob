025d3f35992ba72e3ec9f99b9ef57703c7fce0c0
Masashi <etaonrish13@gmail.com>
2016-04-29 04:17:08 +0000

    add appliance

--- app/models/internship.rb
+++ app/models/internship.rb
@@ -2,4 +2,9 @@ class Internship < ActiveRecord::Base
 	has_many :appliances, dependent: :destroy
 	has_many :pre_workers, through: :appliances, source: :student
 	belongs_to :company
+
+	def applianced_by? student
+		appliances.where(student_id: student.id).exists?
+	end
+			
 end
