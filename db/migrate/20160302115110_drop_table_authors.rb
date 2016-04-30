a79bbc4fd8becf7eadd0b6672f3e94c090f54773
Masashi <etaonrish13@gmail.com>
2016-04-30 07:37:21 +0000

    delete migration files and modify header.html.erb

--- db/migrate/20160302115110_drop_table_authors.rb
+++
@@ -1,5 +0,0 @@
-class DropTableAuthors < ActiveRecord::Migration
-  def change
-  	drop_table :authors
-  end
-end
--- db/migrate/20160302140410_add_column_to_student.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToStudent < ActiveRecord::Migration
-  def change
-    add_column :students, :name, :string
-    add_column :students, :age_id, :integer
-    add_column :students, :gender, :integer
-    add_column :students, :grade_id, :integer
-    add_column :students, :prefecture_id, :integer
-  end
-end
